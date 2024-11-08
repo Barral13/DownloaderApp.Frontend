b55e219d552319566d6444184034c6dcfe6219d6
Dhanglerson Barral <dhanglerson.dev@outlook.com>
2024-11-08 01:32:40 -0300

    Update v3

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DownloaderApp.Frontend;
+using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5223/api/") });
+// Verifique o ambiente para definir a URL base do HttpClient
+if (builder.HostEnvironment.IsDevelopment())
+{
+    // URL para o ambiente de desenvolvimento (localhost)
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7083/") });
+}
+else
+{
+    // URL para o ambiente de produção (Railway)
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://downloaderappapi-production.up.railway.app/") });
+}
 
 await builder.Build().RunAsync();
